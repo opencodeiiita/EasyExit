f265ed4abf7f0854c271cd83463a5f166a4f9726
shrutikarathi6 <122254552+shrutikarathi6@users.noreply.github.com>
2023-12-16 19:42:22 +0530

    Added profile.js and App.js

---
+++ client/component/Profile page/App.js
@@ -0,0 +1,18 @@
+// App.js
+import React from 'react';
+import profile from './profile';
+
+const App = () => {
+  return (
+    <div>
+      <Profile />
+    </div>
+  );
+};
+
+export default App;
+
+// terminal Commands
+// npm init -y   // if you haven't initialized your project with a package.json file
+// npm install react react-dom
+// npm start
---
+++ client/component/Profile page/profile.js
@@ -0,0 +1,30 @@
+// Profile.js
+import React from 'react';
+import './Profile.css'; 
+
+const Profile = () => {
+  return (
+    <div>
+      <ul className="nav-links">
+        <li><a href="#">Easy<span style={{ color: '#8968FF' }}>Exit</span></a></li>
+        <li className="profile-icon" style={{ color: '#8968FF', fontSize: '35px', marginLeft: '87%' }}>
+          <i className='bx bx-user'></i>
+        </li>
+      </ul>
+
+      <div className="profile-card">
+        <div className="image">
+          {/* add profile image */}
+        </div>
+
+        <div className="text-data">
+          <span className="name">Shrutika</span>
+          <span className="email">Shrutikarathi6</span>
+          <span className="prof">Student</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
