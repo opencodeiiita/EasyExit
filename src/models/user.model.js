8700804034edb1ec42f3dbc4a3d8f0a38fd5ca8a
shreeta <126550404+apricity24@users.noreply.github.com>
2023-12-19 16:01:18 +0530

    Update user.model.js

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,9 +1,26 @@
 import { model, Schema } from 'mongoose';
 
 const userSchema = new Schema({
-    
+    name:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    email:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    passwordHash:{
+        type: String,
+        required: true
+    },
+    role:{
+        type: String,
+        required:true
+    }
 }, { timestamps: true });
 
 const User = model('user', userSchema);
 
-export default User;
+export default User;
