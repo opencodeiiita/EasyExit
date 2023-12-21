b45c9c3dd285966864d8f781dfc00442cf9880fc
shahswati098 <146414419+shahswati098@users.noreply.github.com>
2023-12-21 13:21:52 +0530

    Update form.model.js

--- src/models/form.model.js
+++ src/models/form.model.js
@@ -2,7 +2,56 @@ import { Schema, model } from 'mongoose';
 
 const formSchema = new Schema(
   {
-    
+    name: {
+      type: String,
+      required: true,
+    },
+    roll: {
+      type: String,
+      required: true,
+    },
+    sem: {
+      type: String,
+      required: true,
+    },
+    where: {
+      type: String,
+      required: true,
+    },
+    purpose: {
+      type: String,
+      required: true,
+    },
+    transport: {
+      type: String,
+      required: true,
+    },
+    outtime: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+    ownResponsibility: {
+      type: Boolean,
+      default: false,
+    },
+    isUsed: {
+      type: Boolean,
+      default: false,
+    },
+    isAccepted: {
+      type: Boolean,
+    },
+    otp: {
+      type: String
+    },
+    rejectReason: {
+      type: String
+    }
+
   },
   { timestamps: true },
 );
