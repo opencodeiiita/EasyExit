bd954ffffe5bfc9ecd31ab7883ac7f1a9218b3a3
Nandinig24 <nandini24gupta07@gmail.com>
2023-12-24 14:31:53 +0530

    done

--- src/utils/password.js
+++ src/utils/password.js
@@ -1,14 +1,29 @@
-import dotev from 'dotenv';
-
-dotev.config();
-
+import dotenv from 'dotenv';
 import crypto from 'crypto';
 import jwt from 'jsonwebtoken';
 
+dotenv.config();
+
+const SECRET = process.env.SECRET || 'default_secret'; // Use the environment variable or a default value
+
 export function getJwt(object, expiresIn = '30d') {
-  
+  return jwt.sign(object, SECRET, { expiresIn });
 }
 
 export async function hash_password(password) {
-  
+  return new Promise((resolve, reject) => {
+    const salt = crypto.randomBytes(16).toString('hex');
+
+    crypto.scrypt(password, salt, 64, (err, derivedKey) => {
+      if (err) reject(err);
+      resolve(`${salt}:${derivedKey.toString('hex')}`);
+    });
+  });
 }
+
+// You can also use the getJwt function like this:
+const userObject = { userId: '123', username: 'john_doe' };
+const token = getJwt(userObject);
+console.log('JWT Token:', token);
+
+
