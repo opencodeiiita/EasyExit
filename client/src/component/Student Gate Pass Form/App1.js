3949606d3582d4e2febaab4c5e44211d888de57a
shrutikarathi6 <122254552+shrutikarathi6@users.noreply.github.com>
2023-12-24 11:01:35 +0530

    Updated Newgatepass form

---
+++ client/src/component/Student Gate Pass Form/App1.js
@@ -0,0 +1,16 @@
+// src/App1.js
+
+import React from 'react';
+import NewGatePass from './Newwgatepass';
+
+function App1() {
+  return (
+    <div>
+      <Newgatepass />
+    </div>
+  );
+}
+
+// export default App;
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<App1/>)
