3692b094cbc7cf66db0bc5f117c8510fbd16ee2b
shahswati098 <146414419+shahswati098@users.noreply.github.com>
2024-01-06 13:40:42 +0530

    Update admin.middleware.js

--- src/middlewares/admin.middleware.js
+++ src/middlewares/admin.middleware.js
@@ -1,14 +1,35 @@
-import jwt from 'jsonwebtoken';
+try {
+        const authHeader = req.header('Authorization');
 
-import {
-    response_401,
-    response_400,
-    response_500,
-} from '../utils/responseCodes.js';
-import User from '../models/user.model.js';
+        if (!authHeader || !/(Bearer )\w+/.test(authHeader)) {
+            // authorization header is not present or is not of the required format
+            return response_400(res, 'Request is invalid');
+        }
 
-const adminMiddleware = async (req, res, next) => {
-    
-};
+        // extracting the token and verifying it
+        const authToken = authHeader.replace('Bearer ', '');
 
-export default adminMiddleware;
+        let userMongoId;
+
+        try {
+            const { payload } = jwt.verify(authToken, process.env.SECRET); // will throw err if token is invalid or expired
+            req.isAuthenticated = true;
+            userMongoId = payload.id;
+        } catch (err) {
+            // token is invalid or expired
+            return response_401(res, 'Request is unauthorized');
+        }
+
+        // extracting user info from DB
+        const user = await User.findById(userMongoId);
+
+        if (!user || user.role!="Admin") {
+            // user is not present in DB
+            return response_401(res, 'Request is unauthorized!');
+        }
+
+        req.user = user;
+        next();
+    } catch (err) {
+        return response_500(res, 'Internal Server Error', err);
+    }
