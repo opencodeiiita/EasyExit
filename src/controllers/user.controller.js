cfadb3128eaae41d896774bd635c240deb33a463
priyankasharma2823 <153724725+priyankasharma2823@users.noreply.github.com>
2024-01-09 23:47:43 +0530

    Update user password function added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -9,5 +9,14 @@ export async function getUserProfile(req, res) {
 }
 
 export async function updatePassword(req, res) {
-    
+    if (!(req.body.oldPassword && req.body.newPassword))
+        return response_400(res, 'All request parameters not provided');
+    const password = await hash_password(req.body.oldPassword);
+    if (password !== req.user.passwordHash)
+        return response_400(res, 'Wrong Password');
+    if (req.body.newPassword.length < 6)
+        return response_400(res, 'New password length too short');
+    req.user.passwordHash = await hash_password(req.body.newPassword);
+    await req.user.save();
+    return response_200(res, 'Password updated');
 }
